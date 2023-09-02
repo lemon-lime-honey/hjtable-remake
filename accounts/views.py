0147899e501364897b4d14b059569273fee411d7
lemon-lime-honey <bs0308@g.skku.edu>
2023-09-02 17:20:43 +0900

    Add signup view
    - 회원가입 뷰 작성

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import *
 
-# Create your views here.
+
+class signupAPIView(APIView):
+    http_method_names = ['post']
+    permission_classes = (permissions.AllowAny,)
+
+
+    def post(self, *args, **kwargs):
+        serializer = UserSerializer(data=self.request.data)
+
+        if serializer.is_valid():
+            get_user_model().objects.create_user(**serializer.validated_data)
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(
+            status=status.HTTP_400_BAD_REQUEST,
+            data={'errors': serializer.errors}
+        )
