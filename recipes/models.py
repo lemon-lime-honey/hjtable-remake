7a4aa5c9619199958e22e036d06a9b1924f1b398
lemon-lime-honey <bs0308@g.skku.edu>
2023-09-17 14:42:21 +0900

    Add models for app 'recipes'
    - 모델 Ingredient 작성
    - 모델 Recipe 작성
    - 모델 RecipeStep 작성
    - 모델 RecipeIngredient 작성

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="recipe"
+    )
+    content = models.TextField(max_length=1000, blank=True, null=True)
+    category = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    time = models.IntegerField()
+    difficulty = models.IntegerField(choices=(1, 2, 3, 4, 5))
+    ingredients = models.ManyToManyField(Ingredient, through="RecipeIngredient")
+
+
+    def __str__(self):
+        return self.title
+
+
+class RecipeStep(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    detail = models.TextField()
+
+
+    def __str__(self):
+        return self.recipe.title
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=50, blank=True, null=True)
