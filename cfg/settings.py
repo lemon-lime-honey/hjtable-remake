4e2e0d217af37c864acc8927dcbb56fc631213a4
lemon-lime-honey <bs0308@g.skku.edu>
2023-09-26 20:44:58 +0900

    Update DB settings
    - requirements에 mysqlclient 패키지 추가
    - MySQL 연결

--- cfg/settings.py
+++ cfg/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'cfg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['db_name'],
+        'USER': os.environ['db_user'],
+        'PASSWORD': os.environ['db_pw'],
+        'HOST': os.environ['db_host'],
+        'PORT': os.environ['db_port']
     }
 }
 
