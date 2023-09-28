81b923bdea53eaa64f2b5df50101d3b6e6c2a659
lemon-lime-honey <bs0308@g.skku.edu>
2023-09-28 20:08:02 +0900

    Update RecipeViewSet
    - 레시피 등록/수정 시 존재하지 않는 재료를 입력한 경우 처리
    - RecipeViewSet에서 create, update 메서드 재정의

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,29 @@ from .serializers import *
 class RecipeViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
+
+
+    def create(self, request, *args, **kwargs):
+        if request.data.get('ingredients'):
+            for i in range(len(request.data['ingredients'])):
+                ingredient = request.data['ingredients'][i]['ingredient']
+                if isinstance(ingredient, str):
+                    try:
+                        ig = Ingredient.objects.get(name=ingredient).pk
+                    except:
+                        ig = Ingredient.objects.create(name=ingredient).pk
+                    request.data['ingredients'][i]['ingredient'] = ig
+        return super().create(request, *args, **kwargs)
+
+
+    def update(self, request, *args, **kwargs):
+        if request.data.get('ingredients'):
+            for i in range(len(request.data['ingredients'])):
+                ingredient = request.data['ingredients'][i]['ingredient']
+                if isinstance(ingredient, str):
+                    try:
+                        ig = Ingredient.objects.get(name=ingredient).pk
+                    except:
+                        ig = Ingredient.objects.create(name=ingredient).pk
+                    request.data['ingredients'][i]['ingredient'] = ig
+        return super().update(request, *args, **kwargs)
