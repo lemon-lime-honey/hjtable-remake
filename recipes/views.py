8ef701c26fa9c04ad586a15303a249e3826c0cc8
lemon-lime-honey <bs0308@g.skku.edu>
2023-09-29 07:32:48 +0900

    Update RecipeViewSet
    - 메서드 create, update에서 겹치는 로직을 새 함수로 분리

--- recipes/views.py
+++ recipes/views.py
@@ -9,7 +9,7 @@ class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
 
 
-    def create(self, request, *args, **kwargs):
+    def new_ingredient(self, request):
         if request.data.get('ingredients'):
             for i in range(len(request.data['ingredients'])):
                 ingredient = request.data['ingredients'][i]['ingredient']
@@ -19,17 +19,15 @@ class RecipeViewSet(ModelViewSet):
                     except:
                         ig = Ingredient.objects.create(name=ingredient).pk
                     request.data['ingredients'][i]['ingredient'] = ig
+
+        return request
+
+
+    def create(self, request, *args, **kwargs):
+        request = self.new_ingredient(request)
         return super().create(request, *args, **kwargs)
 
 
     def update(self, request, *args, **kwargs):
-        if request.data.get('ingredients'):
-            for i in range(len(request.data['ingredients'])):
-                ingredient = request.data['ingredients'][i]['ingredient']
-                if isinstance(ingredient, str):
-                    try:
-                        ig = Ingredient.objects.get(name=ingredient).pk
-                    except:
-                        ig = Ingredient.objects.create(name=ingredient).pk
-                    request.data['ingredients'][i]['ingredient'] = ig
+        request = self.new_ingredient(request)
         return super().update(request, *args, **kwargs)
