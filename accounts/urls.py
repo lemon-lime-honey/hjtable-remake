2f01e8c9733f0511f37b47333b8ef938abfffa02
lemon-lime-honey <bs0308@g.skku.edu>
2023-10-16 20:51:26 +0900

    Update url configuration
    - 'accounts/token/'을 'accounts/login/'으로
    - 'accounts/token/refresh/'를 'accounts/login/refresh/'로 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='signup'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('password/', ChangePasswordView.as_view(), name='change_password'),
     path('update/', UserUpdateView.as_view(), name='user_update'),
     path('delete/', UserDeleteView.as_view(), name='user_delete'),
